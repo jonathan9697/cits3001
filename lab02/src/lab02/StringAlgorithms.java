1743caec3f7dad76201026a93c8cb1c6ed5b8a3c
Michael Sargeant <22737938@student.uwa.edu.au>
2020-08-07 13:00:40 +0800

    simple RK algorithm complete

--- lab02/src/lab02/StringAlgorithms.java
+++ lab02/src/lab02/StringAlgorithms.java
@@ -25,8 +25,17 @@ public class StringAlgorithms {
 	//CONSTRUCTOR
 	
 	//METHODS
+	
+	/*
+	 * applies naive algorithm to
+	 * find pattern within text
+	 * 
+	 * @param t the text to search
+	 * @param p the pattern to find
+	 * @returns result list of shifts
+	 * 
+	 */
 	List<Integer> naive(int[] t, int[] p) {
-		
 		int n = t.length;
 		int m = p.length;
 		List<Integer> result=new ArrayList<Integer>();
@@ -39,19 +48,52 @@ public class StringAlgorithms {
 		return result;
 	}
 	
-	
-	/**
-	 * @param args
+	/*
+	 * applies Rabin-Karp algorithm to
+	 * find pattern within text
+	 * 
+	 * @param t the text to search
+	 * @param p the pattern to find
+	 * @returns result list of shifts
+	 * 
 	 */
+	List<Integer> rabinKarp(int[] t, int[] p) {
+		int n = t.length;
+		int m = p.length;
+		List<Integer> result=new ArrayList<Integer>();
+		
+		//Rabin-Karp pre-processing
+		int pInt = 0;
+		for (int j=0; j<m; j++) {
+			pInt = pInt*10 + p[j];
+		}
+		System.out.println("pInt: " + pInt);
+		
+		//Rabin-Karp matching
+		for (int s=0; s<n-m+1; s++) {
+				int z = 0;
+				for(int j=0; j<m; j++) {
+					z = z*10 + t[j+s];
+				}
+				if (z != pInt) continue;
+				else result.add(s);
+		}
+		return result;
+	}
 	
 	
+
+	/*
+	 * main method for testing
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] t = {1,  3,  9,  9,  1,  3,  9,  3};
 		int[] p = {3, 9};
 		
 		StringAlgorithms test = new StringAlgorithms();
-		System.out.println(Arrays.toString(test.naive(t, p).toArray()));
+		System.out.println("naive:     " + Arrays.toString(test.naive(t, p).toArray()));
+		System.out.println("rabinKarp: " + Arrays.toString(test.rabinKarp(t, p).toArray()));
 
 	}
 
